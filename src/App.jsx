cf2644bbbf137623e54860405a4c8e4fc9dfd5e1
Dimtiri Lieder <dimitri@lieder.email>
2024-08-16 17:47:18 +0200

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { DataGrid } from '@mui/x-data-grid';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,9 +22,11 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-      </button>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" onClick={() => setCount((count) => count + 1)}>{count}</Button>
+        <Button variant="contained" onClick={() => setCount((count) => count + 1)}>{count}</Button>
+        <Button variant="outlined" onClick={() => setCount((count) => count + 1)}>{count}</Button>
+      </Stack>
       <DataGrid rows={rows} columns={columns} />
     </>
   )
